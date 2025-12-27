06cbf09e75563e6e9c05261eb8c20e2bd9dcd3bd
akash@aks <akash216180@gmail.com>
2025-12-28 01:58:38 +0530

    my commit

--- #prime-or-not.py
+++ #prime-or-not.py
@@ -1,5 +1,16 @@
-#Write a program to check whether a number is a prime number.
+num = int(input("ENTER THE NUMBER THAT YOU WANT TO CHECK: "))
 
-x=5
-x='five '
-print (type(x))
+if num <= 1:
+    print("THE NUMBER YOU ENTER IS NOT A PRIME")
+else:
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break   # stop checking further
+
+    if is_prime:
+        print("THE NUMBER YOU ENTER IS PRIME")
+    else:
+        print("THE NUMBER YOU ENTER IS NOT A PRIME")
